8ae0e7933907add4c1ce137ec3a19c9e9039ca99
Prashant Sugave <prashantsugave125@gmail.com>
2023-11-09 12:31:37 +0530

    initial commit

--- notesapp/src/components/CreateNote.tsx
+++ notesapp/src/components/CreateNote.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import {InputBase, Box, Button, styled, Typography} from '@mui/material'
-import { NoteObject } from '../models/Note'
-import { TITLE_LIMIT, DETAILS_LIMIT } from '../constants/Constant';
-import { v4 as uuid} from 'uuid';
+import { useState } from "react";
+import { InputBase, Box, Button, styled, Typography } from "@mui/material";
+import { NoteObject } from "../models/Note";
+import { TITLE_LIMIT, DETAILS_LIMIT } from "../constants/Constant";
+import { v4 as uuid } from "uuid";
 
 const Container = styled(Box)`
   & > *{
@@ -25,83 +25,87 @@ const Container = styled(Box)`
     position: relative;
     right:40px;
   }
-`
+`;
 const Error = styled(Typography)`
-background: red;
-color: #fff;
-padding: 10px;
-width: 50%;
-`
+  background: red;
+  color: #fff;
+  padding: 10px;
+  width: 50%;
+`;
 const defaultObj = {
-    id:0,
-    title:'',
-    details:'',
-    color:'',
-    date:(new Date().toLocaleString()).toString()
-}
+  id: 0,
+  title: "",
+  details: "",
+  color: "",
+  date: new Date().toLocaleString().toString(),
+};
 
 interface ICreateNoteProps {
-  addNotes:  (note: NoteObject) => void
+  addNotes: (note: NoteObject) => void;
 }
 const CreateNote: React.FC<ICreateNoteProps> = ({ addNotes }) => {
-    const [note, setNote] = useState<NoteObject>(defaultObj);
-    const [error, setError] = useState<string>('');
+  const [note, setNote] = useState<NoteObject>(defaultObj);
+  const [error, setError] = useState<string>("");
 
-    const onValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{
-        // console.log(e.target.name, e.target.value);
-        if(error){
-          setError('');
-        }
-        setNote({ ...note, [e.target.name]: e.target.value})
-        // console.log(note);
+  const onValueChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    // console.log(e.target.name, e.target.value);
+    if (error) {
+      setError("");
     }
+    setNote({ ...note, [e.target.name]: e.target.value });
+    // console.log(note);
+  };
 
-    const onCreateNote = () => {
-      if(!note.title && !note.details){
-        setError('All fields are mandatory');
-        return;
-      }
-        addNotes({ ...note, id: uuid() })
-        setNote(defaultObj)
+  const onCreateNote = () => {
+    if (!note.title && !note.details) {
+      setError("All fields are mandatory");
+      return;
     }
+    addNotes({ ...note, id: uuid() });
+    setNote(defaultObj);
+  };
 
-    return(
-        <Container>
-            <InputBase 
-              placeholder='Title'
-              onChange={(e) => onValueChange(e)}
-              name="title"
-              value={note.title}
-              inputProps={{
-                maxLength: TITLE_LIMIT
-              }}
-            />
-            <Box component="span">{note.title.length}/{TITLE_LIMIT}</Box>
-            <InputBase 
-              placeholder='Details'
-              onChange={(e) => onValueChange(e)}
-              name="details"
-              value={note.details}
-              inputProps={{
-                maxLength: DETAILS_LIMIT
-              }}
-
-            />
-            <Box component="span">{note.details.length}/{DETAILS_LIMIT}</Box>
-            <InputBase
-              type='color'
-              defaultValue={'#F5F5F5'}
-              placeholder='choose color'
-              onChange={(e) => onValueChange(e)}
-              name="color"
-            />
-            <Button 
-              variant="outlined"
-              onClick={() => onCreateNote()}
-            >Create</Button>
-             { error && <Error>{error}</Error>}
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <InputBase
+        placeholder="Title"
+        onChange={(e) => onValueChange(e)}
+        name="title"
+        value={note.title}
+        inputProps={{
+          maxLength: TITLE_LIMIT,
+        }}
+      />
+      <Box component="span">
+        {note.title.length}/{TITLE_LIMIT}
+      </Box>
+      <InputBase
+        placeholder="Details"
+        onChange={(e) => onValueChange(e)}
+        name="details"
+        value={note.details}
+        inputProps={{
+          maxLength: DETAILS_LIMIT,
+        }}
+      />
+      <Box component="span">
+        {note.details.length}/{DETAILS_LIMIT}
+      </Box>
+      <InputBase
+        type="color"
+        defaultValue={"#F5F5F5"}
+        placeholder="choose color"
+        onChange={(e) => onValueChange(e)}
+        name="color"
+      />
+      <Button variant="outlined" onClick={() => onCreateNote()}>
+        Create
+      </Button>
+      {error && <Error>{error}</Error>}
+    </Container>
+  );
+};
 
 export default CreateNote;
--- notesapp/src/components/Note.tsx
+++ notesapp/src/components/Note.tsx
@@ -1,42 +1,46 @@
-
-
 import { NoteObject } from "../models/Note";
-import {Card, CardContent, Box, Typography, Button, styled} from '@mui/material'
+import {
+  Card,
+  CardContent,
+  Box,
+  Typography,
+  Button,
+  styled,
+} from "@mui/material";
 
 interface INotesProps {
-    note: NoteObject
-    deleteNote: (id: number) => void
-
+  note: NoteObject;
+  deleteNote: (id: number) => void;
 }
 
 const StyledCard = styled(Card)`
-width:400px;
-margin: 20px;
-`
+  width: 400px;
+  margin: 20px;
+`;
 
 const Wrapper = styled(Box)`
-& > button {
-  border: 1px solid #000;
-  background: #fff;
-  margin-top: Spx;
-}`
+  & > button {
+    border: 1px solid #000;
+    background: #fff;
+    margin-top: Spx;
+  }
+`;
 
-const Note: React.FC <INotesProps>= ({note, deleteNote}) => {
-     
-    return(
-        <StyledCard style={{backgroundColor: note.color}}>
-          <CardContent>
-            <Wrapper>
-                <Typography>{note.title}</Typography>
-                <Typography>{note.details}</Typography>
-                <Typography>{note.date}</Typography>
-                <Button variant="outlined" onClick={() => deleteNote(note.id)}>Delete</Button>
-
-            </Wrapper>
-
-          </CardContent>
-        </StyledCard>
-    )
-}
+const Note: React.FC<INotesProps> = ({ note, deleteNote }) => {
+  return (
+    <StyledCard style={{ backgroundColor: note.color }}>
+      <CardContent>
+        <Wrapper>
+          <Typography>{note.title}</Typography>
+          <Typography>{note.details}</Typography>
+          <Typography>{note.date}</Typography>
+          <Button variant="outlined" onClick={() => deleteNote(note.id)}>
+            Delete
+          </Button>
+        </Wrapper>
+      </CardContent>
+    </StyledCard>
+  );
+};
 
 export default Note;
